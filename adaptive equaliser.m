28cb848e9d3d055bf7eb8e1e4961f63091f3c896
kathalan <kadhalan347@gmail.com>
2025-05-11 20:49:30 +0530

    Update adaptive equaliser.m

--- adaptive equaliser.m
+++ adaptive equaliser.m
@@ -1,16 +1,58 @@
 % Simulation of adaptive equalizer
-clc;
-clear;
-n = 1000;
-x = randi([0 1],1,n);
-modSig = 2*x - 1;
-chan = [0.9 0.2];
-rxSig = filter(chan,1,modSig);
-rxSig = awgn(rxSig,10,'measured');
-eq = adaptfilt.lms(32,0.01);
-[d, e] = adaptfilt.lms(32,0.01);
-[~,e] = filter(eq,modSig,rxSig);
-plot(e);
-title('Error Signal');
-xlabel('Samples');
-ylabel('Amplitude');
+ADAPTIVE EQUALIZER:
+
+clear all,
+
+close all
+
+mse=[]:
+
+N=1000;
+
+sysorder 20,
+
+x=randn(N, 1);
+
+b=fir1(sysorder-1,0.5):
+
+n=0.1*randn(N,1);
+
+d=filter(b. 1,x)+n;
+
+steps=[0.008,0.02,0.05);
+
+for i=1:length(steps)
+
+temp=0;
+
+w=zeros(sysorder, 1);
+
+for n=sysorder:N
+
+u=x(n:-1:n-sysorder+1);
+
+y(n)= w"u;
+
+e(n)=d(n)-y(n);
+
+w=w+steps(i)*u*e(n);
+
+temp-temp+(e(n)^2);
+
+mse(i,n)=temp/n;
+
+end;
+
+end;
+
+plot(1:n,mse (1.:),'-b', 1:n, mse (2,:),'-m', 1:n,mse(3,:),'-r');
+
+axis([1 N-0.2 0.5]);
+
+title('OUTPUT');
+
+xlabel('NO of itertion');
+
+ylabel('Mean square Error');
+
+legend('stepsiz=0.008', 'step size=0.02', 'step size=0.05');
