f5911896ea516e95dd602af743c9a9ba598a81a3
kathalan <kadhalan347@gmail.com>
2025-05-11 21:50:08 +0530

    Create cycliccodes.m

---
+++ cycliccodes.m
@@ -0,0 +1,36 @@
+%Cyclic codes
+cdc;
+
+clear all;
+
+SNR= [-20:1:20]
+
+k=4;
+
+n=7;
+
+for j=1:1:length(SNR)
+
+msg=randsrc(10000, k, [0 1]);
+
+enc_sig=encode(msg,n,k,'cyclic/binary');
+
+msg_tx=pskmod(enc_sig,2);
+
+msg_rx=awgn(msg_tx,SNR(j));
+
+dec_sig=decode(msg_demod,n,k,'cyclic/binary);
+
+msg_demod=pskdemod(msg_rx,2);
+
+[errorbit, ratiobit]=biterr(msg,dec_sig);
+
+ber(j)=ratiobit;
+
+end;
+
+semilogy(SNR,ber);
+
+xlabel('SNR');
+
+ylabel('BER analysis of cyclic Code");
